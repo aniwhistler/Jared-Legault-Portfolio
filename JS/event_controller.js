8f3a912cebeaf26ae4c1963a809c915a64349dd6
Jared Legault <aniwhistler@gmail.com>
2026-07-20 22:53:45 +0900

    Buttons now work to fade in. Needed to get rid of the fade out. The project button also works, and they have hover and clicked styles as well. Added a simple footer.

--- JS/event_controller.js
+++ JS/event_controller.js
@@ -1,14 +1,41 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["intro", "project", "welcome"]
+  static targets = ["intro", "project", "welcome", "ibutton", "pbutton"]
   connect() {
-    console.log("Hello there.")
   }
 
   introclicked() {
-    this.welcomeTarget.classList.add("fade-out")
-    this.introTarget.classList.remove("d-none")
-    this.introTarget.classList.add("fade-in")
+    this.welcomeTarget.classList.add("fade-out");
+    this.welcomeTarget.classList.add("d-none");
+
+    this.introTarget.classList.add("fade-in");
+    this.introTarget.classList.remove("d-none");
+
+    this.projectTarget.classList.add("d-none");
+
+    this.ibuttonTarget.classList.add("button-clicked");
+    this.ibuttonTarget.classList.remove("main-button");
+
+    this.pbuttonTarget.classList.remove("fade-in");
+    this.pbuttonTarget.classList.remove("button-clicked");
+    this.pbuttonTarget.classList.add("main-button");
+  }
+
+  projectclicked() {
+    this.welcomeTarget.classList.add("fade-out");
+    this.welcomeTarget.classList.add("d-none");
+
+    this.projectTarget.classList.add("fade-in");
+    this.projectTarget.classList.remove("d-none");
+
+    this.introTarget.classList.add("d-none");
+
+    this.pbuttonTarget.classList.add("button-clicked");
+    this.pbuttonTarget.classList.remove("main-button");
+
+    this.ibuttonTarget.classList.remove("fade-in");
+    this.ibuttonTarget.classList.remove("button-clicked");
+    this.ibuttonTarget.classList.add("main-button");
   }
 }
